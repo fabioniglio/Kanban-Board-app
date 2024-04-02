56348959b8c2e3d8f7426b6e137fe01e2b9b1b99
Fabio Niglio <fabio_niglio@hotmail.com>
2024-04-02 17:52:29 +0200

    text about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,14 +2,29 @@ import "./About.css";
 
 function About() {
   return (
-    <div className="About">
-      <div className="Profile">
-        <div className="profile-circle"></div>
-        <p>Fabio Niglio</p>
+    <div className="contentAbout">
+      <div className="appDetails">
+        <div className="appText">
+          <p>Kanban App!</p>
+          <p>
+            Kanban App is your go-to solution for streamlined task management.
+            Designed for both individuals and teams, it offers a simple yet
+            powerful way to organize, track, and update tasks. Quickly add tasks
+            to your "To Do" column, and as you make progress, move them through
+            "Doing" to "Done." This visual approach not only keeps your workflow
+            organized but also visibly tracks your accomplishments.
+          </p>
+        </div>
       </div>
-      <div className="Profile">
-        <div className="profile-circle"></div>
-        <p>Fabio Niglio</p>
+      <div className="About">
+        <div className="Profile">
+          <div className="profile-circle"></div>
+          <p>Fabio Niglio</p>
+        </div>
+        <div className="Profile">
+          <div className="profile-circle"></div>
+          <p>Fabio Niglio</p>
+        </div>
       </div>
     </div>
   );
