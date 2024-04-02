acc7673aaf238e428ec6145dec85be305b7c78d6
Fabio Niglio <fabio_niglio@hotmail.com>
2024-04-02 16:44:48 +0200

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,16 @@
+import "./Navbar.css";
+
 function Navbar() {
-  return <div className="Navbar">Navbar</div>;
+  return (
+    <div className="Navbar">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
