0f9f820bb63751e066067e24339257e9d973a235
Jinisha Patel <jinishakheni@gmail.com>
2024-04-03 23:05:06 +0200

    Added style to error page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,9 +1,11 @@
-function ErrorPage() {
+import classes from "../styles/ErrorPage.module.css";
+
+const ErrorPage = () => {
   return (
-    <div>
+    <div className={classes.container}>
       <h1>Error: Page Not found</h1>
     </div>
   );
-}
+};
 
 export default ErrorPage;
